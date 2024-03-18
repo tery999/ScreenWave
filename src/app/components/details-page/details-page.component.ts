cbc3b22c76f3fa303122aaea6b43edd04e67aa85
tery999 <51335465+tery999@users.noreply.github.com>
2024-03-18 14:19:41 +0200

    details styling + see more
    added see more function, which expands the summary if its more than 2000 characters. Used the slice pipe

--- src/app/components/details-page/details-page.component.ts
+++ src/app/components/details-page/details-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movies } from 'src/app/interfaces/Movies';
 import { MovieServiceService } from 'src/app/services/movie-service.service';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-details-page',
@@ -9,14 +10,25 @@ import { MovieServiceService } from 'src/app/services/movie-service.service';
   styleUrls: ['./details-page.component.css']
 })
 export class DetailsPageComponent implements OnInit{
-  currentMovie: Movies|undefined;
+  faCalendarDays = faCalendarDays;
+  currentMovie: Movies | undefined;
+  summary:string = "";
   constructor(private route:ActivatedRoute , private movieService:MovieServiceService) {
 
   }
+
+  seeMore:boolean = false;
+
+  seeMoreClickFunction() {
+    console.log("Button Clicked");
+    this.seeMore = !this.seeMore;
+  }
+
   ngOnInit(): void {
     const currentId = this.route.snapshot.params["id"];
     this.movieService.getOneMovie(currentId).subscribe ( (movie) => {
       this.currentMovie = movie;
+      this.summary = this.currentMovie.summary as string
     })
   }
 }
