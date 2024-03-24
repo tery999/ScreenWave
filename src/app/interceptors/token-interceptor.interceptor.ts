0389dd9795e457ac3b4aa45f035f808fa9cfdb0c
tery999 <51335465+tery999@users.noreply.github.com>
2024-03-24 15:26:38 +0200

    token interceptor
    If status is 401, the user will be redirected to the Login page.

--- src/app/interceptors/token-interceptor.interceptor.ts
+++ src/app/interceptors/token-interceptor.interceptor.ts
@@ -3,14 +3,17 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { UserServiceService } from '../services/user-service.service';
+import { Route, Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor( private userService: UserServiceService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let localToken = localStorage.getItem('token');
@@ -23,6 +26,16 @@ export class TokenInterceptorInterceptor implements HttpInterceptor {
         )
       })
     }
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError( (Response: HttpErrorResponse) => {
+        if (Response.status == 401) {
+          localStorage.removeItem("token");
+          this.userService.userSubject.next(false);
+          console.log("THIS IS THE RESPONSE IN INTERCEPTOR", Response)
+          this.router.navigateByUrl("/Login");
+        }
+        return throwError(() => Response)
+      })
+    );
   }
 }
